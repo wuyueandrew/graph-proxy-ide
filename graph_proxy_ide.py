1a1de4b30bcdc36abf4c8a1c78948592ff7a5614
wuyue <wuyue_andrew@outlook.com>
2022-02-24 16:09:30 +0800

    add svc

--- graph_proxy_ide.py
+++ graph_proxy_ide.py
@@ -13,6 +13,8 @@ class k8s_launcher(object):
     def __init__(self) -> None:
         with open("/home/graphscope/gs/graph-ide.yaml") as f:
             self._dep = yaml.safe_load(f)
+        with open("/home/graphscope/gs/graph-svc.yaml") as f:
+            self._svc = yaml.safe_load(f)
         config.load_incluster_config()
         self._v1_api = client.CoreV1Api()
         self._app_api = client.AppsV1Api()
@@ -20,7 +22,12 @@ class k8s_launcher(object):
     def submit_ide_task(self,):
         logger.info("=====graph proxy ide submit task=====")
         cp_dep = copy.deepcopy(self._dep)
+
+        resp_service = self._v1_api.create_namespaced_service(body=self._svc, namespace="graph-manager")
+
         resp_deployment = self._app_api.create_namespaced_deployment(body=cp_dep, namespace="graph-manager")
+
+
 
     def kill_task(self):
         logger.info("=====graph proxy ide kill task=====")
